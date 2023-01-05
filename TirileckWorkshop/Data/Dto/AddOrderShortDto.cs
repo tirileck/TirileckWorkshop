b1591c1bfbb161855884d4949f9296095eb2eee8
sasha <you.sank@mail.ru>
2023-01-06 01:24:25 +0300

    Order form

---
+++ TirileckWorkshop/Data/Dto/AddOrderShortDto.cs
@@ -0,0 +1,18 @@
+﻿namespace TirileckWorkshop.Data.Dto;
+
+public class AddOrderShortDto
+{
+    public string FIO { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Description { get; set; }
+
+    public string DeviceName { get; set; }
+
+    public long? DeviceTypeId { get; set; }
+
+    public long? WorkshopId { get; set; }
+}
--- TirileckWorkshop/Data/Models/Order.cs
+++ TirileckWorkshop/Data/Models/Order.cs
@@ -10,6 +10,8 @@ public class Order
 
     public string Email { get; set; }
 
+    public string PhoneNumber { get; set; }
+
     public string Description { get; set; }
 
     public string DeviceName { get; set; }
