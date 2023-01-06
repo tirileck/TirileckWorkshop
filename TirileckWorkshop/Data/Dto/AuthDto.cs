1bdf396edafa8692f003ed6e6c80cdeef065013c
sasha <you.sank@mail.ru>
2023-01-06 22:38:26 +0300

    Auth front

---
+++ TirileckWorkshop/Data/Dto/AuthDto.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace TirileckWorkshop.Data.Dto;
+
+public class AuthDto
+{
+    public string Login { get; set; }
+
+    public string Password { get; set; }
+}
+
+public static class AuthDtoExtensions
+{
+    public class AuthDtoValidator : AbstractValidator<AuthDto>
+    {
+        public AuthDtoValidator()
+        {
+            RuleFor(x => x.Login)
+                .NotEmpty()
+                .WithMessage("Введите логин!");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Введите пароль!")
+                .Length(8, 64)
+                .WithMessage("Пароль должен содержать минимум 8 символов");
+        }
+    }
+} 
