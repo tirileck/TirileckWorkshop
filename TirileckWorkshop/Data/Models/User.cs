58c27d2aa4c12ce6869fe339f654c2d6879f4c03
sasha <you.sank@mail.ru>
2023-01-06 23:39:26 +0300

    Auth front

--- TirileckWorkshop/Data/Models/User.cs
+++ TirileckWorkshop/Data/Models/User.cs
@@ -11,6 +11,14 @@ public class User
     public string ModdleName { get; set; }
 
     public string Position { get; set; }
+    
+    public string PhoneNumber { get; set; }
+
+    public string Email { get; set; }
+
+    public string Login { get; set; }
+
+    public string Password { get; set; }
 
     public Workshop Workshop { get; set; }
     public long? WorkshopId { get; set; }
